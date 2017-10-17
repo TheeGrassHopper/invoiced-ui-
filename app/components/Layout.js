acec213321f4644a03df02f00e7b7f7ff5bc83cc
Alwan Mortada <almortada88@gmail.com>
2017-10-17 12:16:24 -0500

    react clean up

---
+++ app/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Contact from './Contact'
+import data from './data'
+
+class Layout extends React.Component {
+  render() {
+    return(
+      <div id='Layout' className='pure-g'>
+      {data.map(info =>
+	      <Contact key={info.id} {...info} />
+      	)}
+      </div>
+
+    );
+  }
+}
+
+export default Layout;
---
+++ app/components/data.js
@@ -0,0 +1,7 @@
+const data = [
+	{name: "Zack Siri", email: "zack@codemy.net", id: 1},
+	{name: "Mike Siri", email: "Mike@codemy.net", id: 2},
+	{name: "Tom Siri", email: "Tom@codemy.net", id: 3}
+];
+
+export default data;
