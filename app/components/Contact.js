1021b58ae7bd40ef52d5d870c989742b932ca248
Alwan Mortada <almortada88@gmail.com>
2017-10-17 13:57:52 -0500

    clean up

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-const Contact = (props) => {
+const Contact = props => 
 	<div className="pure-u-1-3">
-		<h2> {props.name} </h2>
-		<p> {props.email} </p>
-	</div>
-}
+		<h2>{props.name}</h2>
+		<p>{props.email}</p>
+	</div>;
 
 export default Contact;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 import 'purecss/build/pure.css';
 import './style.css';
 
-
 import React from 'react';
 import { render } from 'react-dom';
 
-
-import Layout from './components';
+import Layout from './components/index';
 
 render(<Layout />, document.getElementById('app'));
