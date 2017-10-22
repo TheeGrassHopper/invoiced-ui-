3125e35bdc5385cc6e65df0a6d080e4f18dcccd1
alwan mortada <alwanmortada@alwans-MacBook-Pro.local>
2017-10-21 22:49:06 -0500

    React: Adding a Form

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -20,18 +20,33 @@ class Layout extends React.Component {
 		})
 	}
 
+	newContact = () =>
+  	<div className='pure-g'>
+    	<div className='pure-u-12-24'>
+    		<from className="pure-form">
+    			<fieldset>
+    				<legend>New Contact</legend>
+    				<input type='email' placeholder='example@example.com' />
+    				<input type='text' placeholder='Name'/>
+    				<button type="submit" className="pure-button pure-button-primary">Add</button>
+    			</fieldset>
+    		</from>
+    	</div>
+    </div>;
+	
 
   render() {
     return(
-    	<div id='Layout'>
-    	<a href="#" className="pure-button" onClick={this.addContact}>Add Contact</a>
+    	<div>
+	    	<div id='Layout'>
+	    	{this.newContact()}
+		    </div>
 	      <div className='pure-g'>
 	      {this.state.contacts.map(info =>
 		      <Contact key={info.id} {...info} />
 	      	)}
 	      </div>
-      </div>
-
+	    </div>
     );
   }
 }
